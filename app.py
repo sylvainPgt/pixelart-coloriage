de9225fbd4047921ac483f34a5cb1026baf2f783
Sylvain <prigent.S@gmail.com>
2025-03-23 15:12:14 +0100

    💄 Ajoute logo, fond de page et affichage amélioré de la taille des points

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
+
 def generate_pixel_art(image: Image.Image, grid_size=20, point_radius=0.12):
     # Redimensionnement
     img = image.convert("RGB").resize((grid_size, grid_size), Image.NEAREST)
@@ -37,16 +43,26 @@ def generate_pixel_art(image: Image.Image, grid_size=20, point_radius=0.12):
 
 # Interface Streamlit
 st.title("Pixel Art à colorier")
+st.image("assets/logo.png", width=120)
 
 uploaded_file = st.file_uploader("Choisis une image", type=["jpg", "jpeg", "png"])
 
 grid_size = st.slider("Taille de la grille (nombre de cases)", 5, 50, 20)
-point_radius = st.slider("Taille des points", 0.05, 0.3, 0.12)
+point_radius = st.slider(
+    "Taille des points (0 = invisible, 1 = toute la case)",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.12,
+    step=0.01,
+    format="%.2f"
+)
+
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Image d'origine", use_column_width=True)
+    st.image(image, caption="Image d'origine", use_container_width=True)
+    
 
     buf = generate_pixel_art(image, grid_size=grid_size, point_radius=point_radius)
-    st.image(buf, caption="Grille à colorier", use_column_width=True)
+    st.image(buf, caption="Grille à colorier", use_container_width=True)
     st.download_button("Télécharger l'image", buf, file_name="grille_coloriage.png", mime="image/png")
